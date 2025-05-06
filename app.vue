feb653eb34fe68cee71d410e583f1001ae543e57
corb3nik <ian@atvh.dev>
2025-05-06 12:34:03 -0400

    Add plausible script

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 import '@/assets/css/index.css';
+
+useHead({
+  script: [{
+    src: 'https://atlas.caido.io/stats/script.js',
+    defer: true,
+    'data-api': 'https://atlas.caido.io/stats/event',
+    'data-domain': 'atlas.caido.io'
+  }]
+});
 </script>
 
 <template>
